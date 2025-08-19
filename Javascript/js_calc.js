ff2781ea6b810880d5bb27e94f5404ee92ad0f6a
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-08-19 18:36:18 +0530

    Add calculator functionality and integrate prompt-sync for user input

---
+++ Javascript/js_calc.js
@@ -0,0 +1,12 @@
+n1=63
+n2=45
+const prompt = require('prompt-sync')();
+n1=Number(prompt("Enter first number: "))
+n2=Number(prompt("Enter second number: "))
+console.log(n1+n2)
+console.log(n1-n2)
+console.log(n1*n2)
+console.log(n1/n2)
+console.log(Math.floor(n1/n2))
+console.log(n1%n2)
+console.log(n1**n2)
