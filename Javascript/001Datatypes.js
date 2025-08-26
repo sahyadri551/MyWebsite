c6a27b8244d5442ee3d4d4851e1530aca02238b0
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-08-26 18:41:56 +0530

    Add JavaScript files demonstrating array methods, data types, functions, string methods, and event handling

---
+++ Javascript/001Datatypes.js
@@ -0,0 +1,22 @@
+
+let x = 16
+let y = 15.9
+let car='Tata';
+let str="it's all right";
+let str1='My name is "Ajay"';
+let a=BigInt("123456789000000000");
+let b=true;
+let c=false;
+let k;
+let j=null;
+//let i=$;// symbol data type
+//object type
+const person={firstname: "Hem", lastname: "Raj"};
+//array
+const cars=["Tata", "Mahindra", "Maruti"];
+//date object
+const date=new Date("2025-10-05");
+console.log(typeof x);
+let p=17 + "Text";// 17Text
+let p2=17 + 5 + "Text";// 22Text
+let p3="Text" + 17 + 5;// Text175
---
+++ Javascript/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+str.matchAll(/a/g)
